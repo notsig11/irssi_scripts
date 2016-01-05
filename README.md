bb38957e27ed2530cf7e333ff1ea3a3646cc039b
Lee Helpingstine <sig11@reprehensible.net>
2016-01-05 11:47:05 -0600

    My first IRSSI script. A Crappy /giphy searcher.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # irssi_scripts
 irssi scripts I've written... Who needs slack?  
+
+###`giphy.py`###
+simple script to retrieve the first hit from a giphy search.
+usage:
+```
+/load giphy.py
+/giphy "search terms"
+```
+lolz
---
+++ giphy.pl
@@ -0,0 +1,38 @@
+#use strict;
+use URI::Escape;
+use HTTP::Tiny;
+use vars qw($VERSION %IRSSI);
+use JSON::PP;
+use Data::Dumper;
+use Irssi;
+
+my $json = JSON::PP->new->allow_nonref;
+
+$VERSION = '0.0.1';
+%IRSSI = (
+	name => 'giphy',
+	authors => 'Lee Helpingstine',
+  contact => 'sig11@reprehensible.net',
+	description => 'Searches giphy to emulate slack\'s /giphy command.'
+);
+
+my $api_key = 'dc6zaTOxFJmzC';
+
+
+sub searchGiphy {
+	my ($search, $server, $dest) = @_;
+
+	my $url = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=1&q=" . uri_escape($search);
+	my $http = HTTP::Tiny->new();
+	my $response = $http->get($url);
+	my $obj = $json->decode($response->{content});
+
+	my $link = $obj->{data}[0]->{url};
+
+	if ($dest->{type} eq "CHANNEL" || $dest->{type} eq "QUERY") {
+		$dest->command("/msg " . $dest->{name} . " " . $link);
+	}
+
+}
+
+Irssi::command_bind('giphy', \&searchGiphy);
