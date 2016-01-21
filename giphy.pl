4e497b4dbc289f04e42f428b8f997d23be957a5d
David Webber <davewebber@gmail.com>
2016-01-21 11:38:04 -0500

    made it random

--- giphy.pl
+++ giphy.pl
@@ -8,11 +8,11 @@ use Irssi;
 
 my $json = JSON::PP->new->allow_nonref;
 
-$VERSION = '0.0.1';
+$VERSION = '0.0.2';
 %IRSSI = (
 	name => 'giphy',
 	authors => 'Lee Helpingstine',
-  contact => 'sig11@reprehensible.net',
+    contact => 'sig11@reprehensible.net',
 	description => 'Searches giphy to emulate slack\'s /giphy command.'
 );
 
@@ -22,15 +22,15 @@ my $api_key = 'dc6zaTOxFJmzC';
 sub searchGiphy {
 	my ($search, $server, $dest) = @_;
 
-	my $url = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=1&q=" . uri_escape($search);
+	my $url = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q=" . uri_escape($search);
 	my $http = HTTP::Tiny->new();
 	my $response = $http->get($url);
 	my $obj = $json->decode($response->{content});
 
-	my $link = $obj->{data}[0]->{url};
+    my $link = $obj->{data}[int(rand(scalar(keys $obj->{data})))]->{url};
 
 	if ($dest->{type} eq "CHANNEL" || $dest->{type} eq "QUERY") {
-		$dest->command("/msg " . $dest->{name} . " " . $link);
+        $dest->command("/msg " . $dest->{name} . " GIPHY $search " . $link);
 	}
 
 }
